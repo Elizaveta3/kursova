54c79030070bd6a5a3334a6060c9ffdaa22a2850
bezklinskalera <114621202+bezklinskalera@users.noreply.github.com>
2024-04-15 14:32:07 +0300

    salt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import { validationResult } from 'express-validator';
 
@@ -14,19 +15,28 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req,res) => {
+app.post('/auth/register', registerValidation, async (req,res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json(errors.array());
     }
+
+    const password = req.body.password;
+     
+    //Шифрування паролю
+
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
     const doc = new AccountModel({
         email: req.body.email,
-        passwordHash: req.body.password,
+        passwordHash,
         userName: req.body.userName,
     });
-    res.json({
-        success: true,
-    });
+
+    const account = await doc.save();
+
+    res.json(account);
 });
 
 app.listen(8083, (err) => {
