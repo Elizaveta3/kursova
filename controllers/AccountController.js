9a07aaf53c2d295316b2d94bc049c46411c5227d
bezklinskalera <114621202+bezklinskalera@users.noreply.github.com>
2024-04-22 13:55:23 +0300

    ww

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { validationResult } from 'express-validator';
 import AccountModel from '../models/Account.js';
 
