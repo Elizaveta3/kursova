2593b1a9f93c672ab53d86347339260f6c6db5bb
bezklinskalera <114621202+bezklinskalera@users.noreply.github.com>
2024-04-29 21:11:20 +0300

    lera work

--- caloCheckfront/src/pages/authPage.js
+++ caloCheckfront/src/pages/authPage.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header/Header';
 
 export const AuthPage = () => {
-    return(
-        <div>
-            <h1>AuthPage</h1>
-        </div>
-    )
-
-}
+    return (
+        <>
+            <body className="page_enter">
+                <Header />
+                <main className="main">
+                    <section>
+                        <form className="form_enter">
+                            <h1>Вхід</h1>
+                            <fieldset className="form_enter_wrap">
+                                <p className="text_input">Введіть дані.</p>
+                                <div className="form_input">
+                                    <input type="text" className="form_input_field" placeholder="Username" />
+                                    <input type="password" className="form_input_field" placeholder="Password" />
+                                </div>
+                                <p className="form_button">
+                                    <button type="submit" className="submit_button">Увійти</button>
+                                </p>
+                            </fieldset>
+                        </form>
+                    </section>
+                </main>
+            </body>
+        </>
+    );
+};
