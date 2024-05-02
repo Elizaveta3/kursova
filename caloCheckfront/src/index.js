b1de7237c84ca4159d06400f6f2bf179d88a29e2
Elizaveta3 <voronkova.lz2005@gmail.com>
2024-05-02 11:32:54 +0300

    before redux

--- caloCheckfront/src/index.js
+++ caloCheckfront/src/index.js
@@ -6,15 +6,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import store from './redux/store' 
+// import store from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      
+      <Provider store={store}>
         <App />
-      
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
