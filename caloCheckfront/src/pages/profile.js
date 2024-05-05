f3050f55e1db835e439bfc56e8382a0124c553a7
Elizaveta3 <voronkova.lz2005@gmail.com>
2024-05-05 10:01:43 +0300

    delete white stripe

--- caloCheckfront/src/pages/profile.js
+++ caloCheckfront/src/pages/profile.js
@@ -13,17 +13,38 @@ export const Profile = () => {
         navigate('/auth');
     };
     return (
-        <div className="page_profile">
-            <HeaderProfile click1={handleGoToDiary} click2={handleGoToAuthPage} child1="Щоденник" child2="Вийти"></HeaderProfile>
-
-            <section className="profile">
-                <div className="form_profile">
-
-                </div>
-
-            </section>
-
-        </div>
+        <>
+        <body className="page_profile">
+        <HeaderProfile click1={handleGoToDiary} click2={handleGoToAuthPage} child1="Щоденник" child2="Вийти"></HeaderProfile>
+            <main className="main">
+                <section>
+                    <div className="form_profile">
+                        {/* <h1>Вхід</h1>
+                        <fieldset className="form_enter_wrap">
+                            <p className="text_input">Введіть дані.</p>
+                            <div className="form_input">
+                                <input type="text" className="form_input_field" placeholder="Username" id='userName' onChange={handleChange}/>
+                                <input type="password" className="form_input_field" placeholder="Password" id='password'onChange={handleChange}/>
+                            </div>
+                            {errorMessage && (
+                                <Alert severity="error">
+                                    {errorMessage}
+                                </Alert>
+                            )}
+                            <p className="form_button">
+                                <Button
+                                    buttonClass="submit_button" type="submit" onSubmit={handleSubmit}
+                                >
+                                    Увійти
+                                </Button>
+                            </p>
+                            
+                        </fieldset> */}
+                    </div>
+                </section>
+            </main>
+        </body>
+    </>
     )
 
 }
