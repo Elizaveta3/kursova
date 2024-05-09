1a0e000584acc69b99a12ec76b33e82bb7e320fa
bezklinskalera <114621202+bezklinskalera@users.noreply.github.com>
2024-05-09 15:24:50 +0300

    add new model

---
+++ models/CaloriesNorm.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const CaloriesNormSchema = new mongoose.Schema({
+    account: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Profile',
+        required: true,
+        unique: true
+    },
+    caloriesNorm: {
+        type: Number,
+        required: true,
+    },  
+},
+{
+    timestamps: true,
+}
+);
+
+export default mongoose.model('CaloriesNorm',CaloriesNormSchema);
