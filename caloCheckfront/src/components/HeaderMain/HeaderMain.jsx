63327cc2d2922635657e1d8d196a5bd42586402e
Elizaveta3 <voronkova.lz2005@gmail.com>
2024-06-20 20:24:28 +0300

    added switcher for language in main header, became more adaptive

--- caloCheckfront/src/components/HeaderMain/HeaderMain.jsx
+++ caloCheckfront/src/components/HeaderMain/HeaderMain.jsx
@@ -1,8 +1,8 @@
 import logo from './LOGO 2.svg'
 import Button from '../Button/Button'
 import './HeaderMain.css'
-import {  useNavigate } from 'react-router-dom';
-
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
 export default function HeaderMain() {
     const navigate = useNavigate();
 
@@ -15,23 +15,34 @@ export default function HeaderMain() {
     const handleGoToContactPage = () => {
         navigate('/contacts');
     };
+    const [currentLanguage, setCurrentLanguage] = useState('ENG');
+    const handleLanguageChange = (language) => {
+        setCurrentLanguage(language);
+    };
     return (<header className="header">
-        <div className="wrapper">
-            <div className="header_wrappper">
+        <div className="wrapper_main">
+            <div className="header_wrappper_main">
                 <div className="header_logo">
                     <a href="/" className="header_logo_link">
                         <img src={logo} alt="logo" className="header_logo_pic" />
                     </a>
                 </div>
-                <nav className="header_nav">
-                    <ul className="header_list">
-                        <li className="header_item">
-                            <Button buttonClass="header_button_main" handleClick={handleGoToRegisterPage}>Sign up</Button>
-                            <Button buttonClass="header_button_main" handleClick={handleGoToAuthPage}>Sign in</Button>
-                            <Button buttonClass="header_button_main" handleClick={handleGoToContactPage}>Contacts</Button>
-                        </li>
-                    </ul>
-                </nav>
+                <div className="language_switcher">
+                    <span className={currentLanguage === 'UKR' ? 'active_language' : ''} onClick={() => handleLanguageChange('UKR')}>УКР</span>
+                    <span> | </span>
+                    <span className={currentLanguage === 'ENG' ? 'active_language' : ''} onClick={() => handleLanguageChange('ENG')}>ENG</span>
+                </div>
+                <div className="container_header_nav">
+                    <nav className="header_nav">
+                        <ul className="header_list">
+                            <li className="header_item">
+                                <Button buttonClass="header_button_main" handleClick={handleGoToRegisterPage}>Sign up</Button>
+                                <Button buttonClass="header_button_main" handleClick={handleGoToAuthPage}>Sign in</Button>
+                                <Button buttonClass="header_button_main" handleClick={handleGoToContactPage}>Contacts</Button>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
     </header>)
